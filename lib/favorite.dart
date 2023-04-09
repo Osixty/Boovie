6e7d777ee795a44b3a2c02330b1f2511652501b5
Osixty <alzidan057@gmail.com>
2023-04-09 15:12:15 +0700

    zidan

---
+++ lib/favorite.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Favorite extends StatelessWidget {
+  const Favorite({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Favorite'),
+      ),
+      
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:boovie/main.dart';
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.movie,
+              size: 100.0,
+              color: Colors.white,
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Text(
+              "boovie",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
